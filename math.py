620104b882af1086d78767c253455fb38f13361a
Quadri <quadri@gmail.com>
2025-09-07 23:36:24 +0100

    updated docstrings

--- math.py
+++ math.py
@@ -1,6 +1,16 @@
 def add_num():
-    """adds two given numbers"""
-    while True:
+     """
+    Divides two given numbers.
+
+    This function prompts the user for two integers, divides the first
+    by the second, and prints the result. If the input is not a number,
+    it will keep asking until valid input is provided.
+
+    :raises ValueError: If the user enters a non-integer value.
+    :raises ZeroDivisionError: If the second number is zero.
+    :return: None
+    """
+     while True:
         try:
             num1 = int(input("Enter first number: "))
             num2 =int(input("Enter second number: " ))
@@ -11,8 +21,18 @@ def add_num():
             print("Please enter a number")
 
 def div_nums():
-    """divides two given numbers"""
-    while True:
+     """
+    Divides two given numbers.
+
+    This function prompts the user for two integers, divides the first
+    by the second, and prints the result. If the input is not a number,
+    it will keep asking until valid input is provided.
+
+    :raises ValueError: If the user enters a non-integer value.
+    :raises ZeroDivisionError: If the second number is zero.
+    :return: None
+    """
+     while True:
         try:
             num1 = int(input("Enter first number: "))
             num2 =int(input("Enter second number: " ))
